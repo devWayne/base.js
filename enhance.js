0c08a3e48ca8734ed57976225a2627708f6142b4
unknown <dev.wayne.ix@gmail.com>
2014-10-07 15:00:48 +0800

    change2$

--- enhance.js
+++ enhance.js
@@ -1,42 +1,49 @@
-var _ = function() {
-
+if($==undefined){
+	var $=function(){}
 }
+(function($){
 
-if (typeof exports !== 'undefined') {
-	if (typeof module !== 'undefined' && module.exports) {
-		exports = module.exports = _;
+	if (typeof exports !== 'undefined') {
+		if (typeof module !== 'undefined' && module.exports) {
+			exports = module.exports = $;
+		}
+		else if (typeof define === 'function' && define.cmd) {
+		    define(function(require, exports, module) {
+		      module.exports = exports = $;
+		    });
+		}
+	    else if (typeof define === 'function' && define.amd) {
+	   		 define('enhance', [], $);
+	  	}
+	  	else{
+	  		exports.$ = $;
+	  	}	
 	}
-	else if (typeof define === 'function' && define.cmd) {
-    define(function(require, exports, module) {
-      module.exports = exports = _;
-    }
-    else if (typeof define === 'function' && define.amd) {
-    define('enhance', [], _);
-  	}
-	exports._ = _;
-}
 
 
-var ArrProto = Array.prototype,
-	var ObjProto = Object.prototype,
-		var FunProto = function.prototype;
+	var ArrProto = Array.prototype,
+		ObjProto = Object.prototype,
+		FunProto = Function.prototype;
 
 
-var ForEach = ArrProto.forEach,
-	Map = ArrProto.map,
-	Reduce = ArrProto.reduce,
-	ReduceRight = ArrProto.ReduceRight,
-	Filter = ArrProto.filter,
-	Every = ArrProto.every,
-	Some = ArrProto.some,
-	Index = ArrProto.index,
-	LastIndex = ArrProto.lastIndex,
-	IsArray = Array.IsArray;
+	var ForEach = ArrProto.forEach,
+		Map = ArrProto.map,
+		Reduce = ArrProto.reduce,
+		ReduceRight = ArrProto.ReduceRight,
+		Filter = ArrProto.filter,
+		Every = ArrProto.every,
+		Some = ArrProto.some,
+		Index = ArrProto.index,
+		LastIndex = ArrProto.lastIndex,
+		IsArray = Array.IsArray;
 
 
 
-_.each = function(obj, iterator) {
-	var results = {};
-	if (obj == null) return results;
+	$.each = function(obj, iterator) {
+		var results = {};
+		if (obj == null) return results;
 
-}
+	}
+
+})($)
+
